9e272393e455b374c10ad12a359b82ffbd6c4f20
SucessionBridge <thestevelodge@gmail.com>
2025-05-25 21:27:39 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Link to the favicon */}
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
 
