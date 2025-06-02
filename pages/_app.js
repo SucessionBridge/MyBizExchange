81e9e75be14dc8428515176ceb8b2ae8759e4480
SucessionBridge <thestevelodge@gmail.com>
2025-06-01 20:10:55 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-white text-gray-800">
+      <Header />
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
+
