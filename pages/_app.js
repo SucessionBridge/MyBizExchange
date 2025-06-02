732ac7c7fad0b553957d85d06791adf38a6bd5e1
SucessionBridge <thestevelodge@gmail.com>
2025-06-01 20:29:16 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
-import Header from '../components/Header';
+import Header from '../components/Header'; // 👈 import the Header component
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-white text-gray-800">
-      <Header />
-      <Component {...pageProps} />
+      <Header /> {/* 👈 add the Header */}
+      <main className="pt-20 px-4"> {/* padding top to offset fixed header */}
+        <Component {...pageProps} />
+      </main>
     </div>
   );
 }
