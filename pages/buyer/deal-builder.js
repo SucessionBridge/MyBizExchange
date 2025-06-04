e841eb05c12575e4af06d8d1a2effd072e977428
SucessionBridge <thestevelodge@gmail.com>
2025-06-03 20:13:30 -0400

    Create deal-builder.js

---
+++ pages/buyer/deal-builder.js
@@ -0,0 +1,10 @@
+import DealBuilderForm from "@/components/DealBuilderForm";
+
+export default function DealBuilderPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Propose a Subcontractor-Style Deal</h1>
+      <DealBuilderForm />
+    </div>
+  );
+}
