690f2b48ff260b6902dc212cd73efd6cd1ef3991
SucessionBridge <thestevelodge@gmail.com>
2025-07-06 21:45:05 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,31 @@
+// pages/_app.js
 import '../styles/globals.css';
-import Header from '../components/Header'; // 👈 import the Header component
+import Header from '../components/Header';
+import { useEffect } from 'react';
+import { supabase } from '../lib/supabaseClient';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        console.log('Supabase auth event:', event);
+      }
+    );
+
+    return () => {
+      authListener?.subscription?.unsubscribe();
+    };
+  }, []);
+
   return (
     <div className="min-h-screen bg-white text-gray-800">
-      <Header /> {/* 👈 add the Header */}
-      <main className="pt-20 px-4"> {/* padding top to offset fixed header */}
+      <Header />
+      <main className="pt-20 px-4">
         <Component {...pageProps} />
       </main>
     </div>
   );
 }
+
 
 
