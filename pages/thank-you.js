501f21a364352c59e074768651596c5cc2b51c60
SucessionBridge <thestevelodge@gmail.com>
2025-07-14 13:36:22 -0400

    Create thank-you.js
    Add Thank you page

---
+++ pages/thank-you.js
@@ -0,0 +1,8 @@
+export default function ThankYou() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-3xl font-bold text-green-600">✅ Listing Submitted!</h1>
+      <p className="mt-4">Your business listing was submitted successfully. We'll be in touch soon.</p>
+    </div>
+  );
+}
