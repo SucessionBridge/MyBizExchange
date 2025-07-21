7f5b824e510cde53f62293db558ad6b17a17d20a
SucessionBridge <thestevelodge@gmail.com>
2025-07-20 23:52:46 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import supabase from '../lib/supabaseClient'; // ✅ correct
+
 
 export default function Login() {
   const [email, setEmail] = useState('');
