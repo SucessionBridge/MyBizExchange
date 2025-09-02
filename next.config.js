c0bc11cc751445017072df770c7319cb822c69cb
SucessionBridge <thestevelodge@gmail.com>
2025-09-01 20:31:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/images/placeholders/listing-placeholder.jpg',
+        destination: '/images/listing-placeholder.jpg',
+      },
+    ];
+  },
+};
