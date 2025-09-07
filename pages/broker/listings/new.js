a5679d4311ed7a601419bfa43a90da1c76a2fa24
SucessionBridge <thestevelodge@gmail.com>
2025-09-07 13:46:10 -0400

    Update new.js

--- pages/broker/listings/new.js
+++ pages/broker/listings/new.js
@@ -2,7 +2,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import supabase from '../../../lib/supabaseClient';
+import supabase from '../lib/supabaseBrowserClient';
 
 export default function BrokerNewListing() {
   const router = useRouter();
