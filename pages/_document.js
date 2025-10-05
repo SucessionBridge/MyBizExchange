c879d13e6ac011c0ecadeaabb52054dd4c9f6e2b
SucessionBridge <thestevelodge@gmail.com>
2025-10-05 15:54:40 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,23 @@
+// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en" dir="ltr">
         <Head>
-          {/* Link to the favicon */}
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          {/* Keep _document Head minimal. Page-level <Head> should set title, description, OG, canonical, etc. */}
+
+          {/* Performance: preconnect to Supabase for faster image and data loading */}
+          <link
+            rel="preconnect"
+            href="https://yqccgmgycvaahjwbnhze.supabase.co"
+            crossOrigin=""
+          />
+
+          {/* Helpful defaults */}
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="color-scheme" content="light" />
         </Head>
         <body>
           <Main />
@@ -18,4 +29,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
